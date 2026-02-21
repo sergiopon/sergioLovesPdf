934a704b4e9810fca0d3ef4ba84db47ee081911c
sergiopon <1105366347@javerianacali.edu.co>
2026-02-21 10:12:28 -0500

    logica terminada y conexion realizada

--- pdf-merger-api/index.js
+++ pdf-merger-api/index.js
@@ -1,32 +1,31 @@
 const express = require('express')
 const multer = require('multer')
-const app = express()
-const port = 4000
-
-const upload = multer({ dest: 'uploads/' })
+const cors = require('cors')
 const { PDFDocument } = require('pdf-lib')
 const fs = require('fs')
+
+const app = express()
+const port = 4000
+const upload = multer({ dest: 'uploads/' })
+
+app.use(cors())
 
 app.post('/api/pdf/merge', upload.array('pdfs'), async (req, res) => {
   const pdfFinal = await PDFDocument.create()
 
   for (const archivo of req.files) {
-    const bytes = fs.readFileSync(archivo.path)
-
-    const pdf = await PDFDocument.load(bytes)
-
+    const bytesArchivo = fs.readFileSync(archivo.path)
+    const pdf = await PDFDocument.load(bytesArchivo)
     const paginas = await pdfFinal.copyPages(pdf, pdf.getPageIndices())
-
     paginas.forEach(pagina => pdfFinal.addPage(pagina))
   }
 
-  // 7. Convertir el PDF final a bytes
   const bytes = await pdfFinal.save()
-
   res.set('Content-Type', 'application/pdf')
   res.set('Content-Disposition', 'attachment; filename="merged.pdf"')
   res.send(Buffer.from(bytes))
 })
+
 app.listen(port, () => {
   console.log(`Servidor corriendo en puerto ${port}`)
 })
--- pdf-merger-api/pdf-merger-front/src/App.js
+++ pdf-merger-api/pdf-merger-front/src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
+import logo from './logo.jpg';
 import './App.css';
+import axios from 'axios'
 
+
+
+
+import { useState } from 'react'
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const [archivos, setArchivos] = useState([])
+const handleMerge = async () => {
+  const formData = new FormData()
+  // agrega cada archivo al formData con el campo 'pdfs'
+  archivos.forEach(archivo => formData.append('pdfs',archivo))
+
+  const response = await axios.post('http://localhost:4000/api/pdf/merge', formData, {
+    responseType: 'blob'  // importante: le dice a axios que espere un archivo binario
+  })
+
+  // crear un link temporal y hacer click para descargar
+  const url = window.URL.createObjectURL(new Blob([response.data]))
+  const link = document.createElement('a')
+  link.href = url
+  link.setAttribute('download', 'merged.pdf')
+  document.body.appendChild(link)
+  link.click()
+  link.remove()
 }
 
-export default App;
+
+
+
+  const handleChange = (e) => {
+  const seleccionados = Array.from(e.target.files)
+  setArchivos(seleccionados)
+}
+
+  return (
+    <div>
+      <h1>PDF MERGER</h1>
+      <p>Aquí puedes combinar tus PDFs localmente sin pagar nada</p>
+      <input type="file" accept=".pdf" multiple onChange={handleChange} />
+      {archivos.map(archivo => (
+      <p>{archivo.name}</p>
+      ))}
+      {archivos.length >= 2 && (
+      <button onClick = {handleMerge}>Combinar PDFs</button>
+      )}
+    </div>
+  
+  )
+}
+export default App
+
