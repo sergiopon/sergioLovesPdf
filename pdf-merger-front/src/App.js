386620415053708cddb1a2536455acc91a3cfe05
sergiopon <1105366347@javerianacali.edu.co>
2026-02-21 12:30:12 -0500

    se hace la navbar

--- pdf-merger-front/src/App.js
+++ pdf-merger-front/src/App.js
@@ -2,10 +2,12 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Merge from './pages/Merge'
 import Home from './pages/Home'
 import { Link } from 'react-router-dom'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <BrowserRouter>
+      <Navbar className="navbar" />{/* el navbar se muestra en todas las páginas */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/merge" element={<Merge />} />
